c3d7b22688af0a7b909b88a2e6392b6bfada0e31
kyominkim1074 <kyominkim1074@gmail.com>
2022-10-21 16:20:23 +0900

    reviews/update/delete

--- reviews/urls.py
+++ reviews/urls.py
@@ -6,4 +6,7 @@ app_name = "reviews"
 urlpatterns = [
     path("create/", views.create, name="create"),
     path("", views.index, name="index"),
+    path("<int:review_pk>/", views.detail, name="detail"),
+    path("<int:review_pk>/update/", views.update, name="update"),
+    path("<int:review_pk>/delete/", views.delete, name="delete"),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,7 +1,9 @@
+from nntplib import ArticleInfo
 from django.shortcuts import render, redirect
 from reviews.models import Review
 from .forms import ReviewForm
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 # Create your views here.
 
@@ -11,7 +13,10 @@ def create(request):
     if request.method == "POST":
         form = ReviewForm(request.POST)
         if form.is_valid():
-            form.save()
+            review = form.save(commit=False)
+            review.user = request.user
+            review.save()
+            messages.success(request, "작성이 완료되었습니다.")
             return redirect("reviews:index")
     else:
         form = ReviewForm()
@@ -27,3 +32,41 @@ def index(request):
         "reviews": reviews,
     }
     return render(request, "reviews/index.html", context)
+
+
+def detail(request, review_pk):
+    review = Review.objects.get(pk=review_pk)
+    context = {
+        "review": review,
+    }
+    return render(request, "reviews/detail.html", context)
+
+
+@login_required
+def update(request, review_pk):
+    review = Review.objects.get(pk=review_pk)
+    if request.method == "POST":
+        form = ReviewForm(request.POST, instance=review)
+        if form.is_valid():
+            form.save()
+            return redirect("reviews:detail", review.pk)
+    else:
+        form = ReviewForm(instance=review)
+    context = {
+        "review": review,
+        "form": form,
+    }
+    return render(request, "reviews/update.html", context)
+
+
+@login_required
+def delete(request, review_pk):
+    review = Review.objects.get(pk=review_pk)
+    if request.user == review.user:
+        if request.method == "POST":
+            review.delete()
+            messages.success(request, "글이 삭제되었습니다.")
+            return redirect("reviews:index")
+        return render(request, "reviews/detail.html")
+    else:
+        return redirect("reviews:detail", review.pk)
