d7af59f03d19b223d0bbd5a872dbb50260f2ff7c
kyominkim1074 <kyominkim1074@gmail.com>
2022-10-21 11:44:07 +0900

    app/reviews

---
+++ reviews/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from imagekit.models import ProcessedImageField
+from imagekit.processors import ResizeToFill
+from django.conf import settings
+
+# Create your models here.
+
+
+class Review(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=20)
+    content = models.TextField()
+    movie_name = models.CharField(mex_length=20)
+    grade = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
---
+++ reviews/views.py
@@ -0,0 +1,4 @@
+from pyexpat import model
+from django.shortcuts import render, redirext
+
+# Create your views here.
